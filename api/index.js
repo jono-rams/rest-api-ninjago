385a241827e5dcac05d87842fb4d8db6c90dba6a
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-30 17:02:38 -0400

    Created front end app and setup for vercel deployment

--- api/index.js
+++ api/index.js
@@ -7,14 +7,15 @@ require("dotenv").config();
 const db_username = process.env.DB_USERNAME;
 const db_password = process.env.DB_PASSWORD;
 
-const uri = `mongodb+srv://${db_username}:${db_password}@cluster0.ugkk0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 // set up express app
 const app = express();
 
 // connect to MongoDB
+const uri = `mongodb+srv://${db_username}:${db_password}@cluster0.ugkk0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 mongoose.connect(uri, { dbName: "ninjago" });
 mongoose.Promise = global.Promise;
+
+app.use(express.static("public"));
 
 // parse application/json
 app.use(bodyParser.json());
@@ -29,7 +30,4 @@ app.use((err, req, res, next) => {
   res.status(status).send({ error: err.message });
 });
 
-// listen for requests
-app.listen(process.env.port || 3000, function () {
-  console.log("now listening for requests");
-});
+module.exports = app;
