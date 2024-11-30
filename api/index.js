c9084cecb4f36b841de4a554641202730eb9916b
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-30 08:20:09 -0400

    Connect to MongoDB (Atlas) using Mongoose

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// get environment variables
+require('dotenv').config();
+const db_username = process.env.DB_USERNAME;
+const db_password = process.env.DB_PASSWORD;
+
+const uri = `mongodb+srv://${db_username}:${db_password}@cluster0.ugkk0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // set up express app
 const app = express();
+
+// connect to MongoDB
+mongoose.connect(uri, { dbName: "ninjago" });
+mongoose.Promise = global.Promise;
 
 // parse application/json
 app.use(bodyParser.json());
