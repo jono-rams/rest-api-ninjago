e5b030d0313637081f499121d3979c9e5d0ed758
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-29 20:10:53 -0400

    Created routes

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 // set up express app
 const app = express();
 
+// initialize routes
+app.use("/api", require("./routes/api"));
+
 // listen for requests
 app.listen(process.env.port || 3000, function(){
     console.log('now listening for requests');
